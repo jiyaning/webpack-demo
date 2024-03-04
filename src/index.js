7078bd52454ea9463d827058c8a219fe92df0005
ji.yaning <ji.yaning@neolink.com>
2024-03-04 15:56:25 +0800

    webpack 管理资源—回退处理

--- src/index.js
+++ src/index.js
@@ -4,43 +4,15 @@
  * @Author: ji.yaning
  * @Date: 2024-03-04 10:53:51
  * @LastEditors: ji.yaning
- * @LastEditTime: 2024-03-04 15:48:49
+ * @LastEditTime: 2024-03-04 15:55:28
  */
 // 显式要求引入的 lodash 必须存在，然后将它绑定为 _
 import _ from "lodash"
-import "./style.css"
-import Icon from "./icon.png"
-import Data from './data.xml';
-import Notes from './data.csv';
-
-import toml from './data.toml';
-import yaml from './data.yaml';
-import json from './data.json5';
-
-console.log(toml.title); // 输出 `TOML Example`
-console.log(toml.owner.name); // 输出 `Tom Preston-Werner`
-
-console.log(yaml.title); // 输出 `YAML Example`
-console.log(yaml.owner.name); // 输出 `Tom Preston-Werner`
-
-console.log(json.title); // 输出 `JSON5 Example`
-console.log(json.owner.name); // 输出 `Tom Preston-Werner`
 
 function component() {
   const element = document.createElement('div');
 
-  // lodash 现在使用 import 引入
   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-  element.classList.add('hello');
-
-  // 将图像添加到已经存在的 div 中。
-  const myIcon = new Image();
-  myIcon.src = Icon;
-
-  element.appendChild(myIcon);
-
-  console.log(Data);
-  console.log(Notes);
 
   return element;
 }
--- webpack.config.js
+++ webpack.config.js
@@ -4,62 +4,15 @@
  * @Author: ji.yaning
  * @Date: 2024-03-04 13:51:23
  * @LastEditors: ji.yaning
- * @LastEditTime: 2024-03-04 15:48:30
+ * @LastEditTime: 2024-03-04 15:54:20
  */
 const path = require('path');
-const toml = require('toml');
-const yaml = require('yamljs');
-const json5 = require('json5');
+
 
 module.exports = {
   entry: './src/index.js',
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
-  },
-  module: {
-    rules: [
-      {
-        test: /\.css$/i,
-        use: ['style-loader', 'css-loader'],
-      },
-      {
-        test: /\.(png|svg|jpg|jpeg|gif)$/i,
-        type: 'asset/resource',
-      },
-      {
-        test: /\.(woff|woff2|eot|ttf|otf)$/i,
-        type: 'asset/resource',
-      },
-      {
-        test: /\.(csv|tsv)$/i,
-        use: ['csv-loader'],
-      },
-      {
-        test: /\.xml$/i,
-        use: ['xml-loader'],
-      },
-      {
-        test: /\.toml$/i,
-        type: 'json',
-        parser: {
-          parse: toml.parse,
-        },
-      },
-      {
-        test: /\.yaml$/i,
-        type: 'json',
-        parser: {
-          parse: yaml.parse,
-        },
-      },
-      {
-        test: /\.json5$/i,
-        type: 'json',
-        parser: {
-          parse: json5.parse,
-        },
-      },
-    ],
   },
 };
